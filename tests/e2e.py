5a47cbb185b39dfbacdf4cc04138f89402759557
sekine <26474260+kitta65@users.noreply.github.com>
2024-03-20 18:58:41 +0900

    Feature/Thanos (#7)
    * impl thanos operator
    
    * add one test case
    
    * remove unnecessary ci step
    
    * add test cases
    
    * apply for only selection

--- tests/e2e.py
+++ tests/e2e.py
@@ -1,28 +1,55 @@
-from contextlib import contextmanager
 import bpy
+import bmesh
 import addon_utils
+from typing import Literal
+
+Mode = Literal["VERTS", "EDGES", "FACES"]
 
 
-@contextmanager
-def temp_text(string: str):
-    text = bpy.data.texts.new("temp.py")
-    text.from_string(string)
-    try:
-        yield text
-    finally:
-        bpy.data.texts.remove(text)
+def assert_half_wiped_out(mode: Mode):
+    # prepare
+    bpy.ops.mesh.primitive_cube_add()
+    bpy.context.scene.tool_settings.mesh_select_mode = [
+        mode == "VERTS",
+        mode == "EDGES",
+        mode == "FACES",
+    ]
+    bpy.ops.object.mode_set(mode="EDIT")
+    bpy.ops.mesh.select_all(action="SELECT")
+
+    # exec operator
+    bpy.ops.sample.thanos_wipe_out()
+    me = bpy.context.object.data
+    bm = bmesh.from_edit_mesh(me)
+
+    match mode:
+        case "VERTS":
+            expected = 4
+            actual = len(bm.verts)
+        case "EDGES":
+            expected = 6
+            actual = len(bm.edges)
+        case "FACES":
+            expected = 3
+            actual = len(bm.faces)
+
+    # cleanup
+    bm.free()
+    bpy.ops.object.mode_set(mode="OBJECT")
+    bpy.ops.object.delete()
+
+    # assertion
+    if actual != expected:
+        raise Exception(f"{mode}: expected {expected}, actual {actual}")
+
+
+def main():
+    addon_utils.enable("myaddon")
+
+    modes: list[Mode] = ["VERTS", "EDGES", "FACES"]
+    for m in modes:
+        assert_half_wiped_out(m)
 
 
 if __name__ == "__main__":
-    addon_utils.enable("myaddon")
-
-    input_str = "import  bpy\n"
-    expected_str = "import bpy\n"
-    actual_str: str
-
-    with temp_text(input_str) as text:
-        bpy.ops.sample.sample_operator()
-        actual_str = text.as_string()
-
-    if actual_str != expected_str:
-        raise Exception("not formatted!")
+    main()
