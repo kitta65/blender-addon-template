1791a1c2cace31626bcdbb0e0e5743aa3a5c2ef1
dr666m1 <26474260+kitta65@users.noreply.github.com>
2024-03-20 04:54:17 +0000

    improve setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+cd $(dirname $0)/.. # project root
+
 ln -s /blender/4.0/python/bin/python3.10 /usr/local/bin/python
 python -m ensurepip
 python -m pip install fake-bpy-module-4.0
-ln -s /config/workspaces/src /blender/4.0/scripts/addons/myaddon
+ln -s $(pwd)/src /blender/4.0/scripts/addons/myaddon
