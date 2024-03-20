ea424c4c19af1a7acf72247203924b052d30caa9
dr666m1 <26474260+kitta65@users.noreply.github.com>
2024-03-20 09:45:59 +0000

    add test cases

--- tests/e2e.py
+++ tests/e2e.py
@@ -1,24 +1,54 @@
 import bpy
 import bmesh
 import addon_utils
+from typing import Literal
+
+Mode = Literal["VERTS", "EDGES", "FACES"]
+
+
+def assert_half_wiped_out(mode: Mode):
+    # prepare
+    bpy.ops.mesh.primitive_cube_add()
+    bpy.context.scene.tool_settings.mesh_select_mode = [
+        mode == "VERTS",
+        mode == "EDGES",
+        mode == "FACES",
+    ]
+    bpy.ops.object.mode_set(mode="EDIT")
+
+    # exec operator
+    bpy.ops.sample.thanos_wipe_out()
+    me = bpy.context.object.data
+    bm = bmesh.from_edit_mesh(me)
+
+    match mode:
+        case "VERTS":
+            expected = 4
+            actual = len(bm.verts)
+        case "EDGES":
+            expected = 6
+            actual = len(bm.edges)
+        case "FACES":
+            expected = 3
+            actual = len(bm.faces)
+
+    # cleanup
+    bm.free()
+    bpy.ops.object.mode_set(mode="OBJECT")
+    bpy.ops.object.delete()
+
+    # assertion
+    if actual != expected:
+        raise Exception(f"{mode}: expected {expected}, actual {actual}")
 
 
 def main():
-    bpy.ops.mesh.primitive_cube_add()
-    bpy.context.scene.tool_settings.mesh_select_mode = [True, False, False]  # vetex
-    bpy.ops.object.mode_set(mode="EDIT")
-    bpy.ops.sample.thanos_wipe_out()
+    addon_utils.enable("myaddon")
 
-    me = bpy.context.object.data
-    bm = bmesh.from_edit_mesh(me)
-    n_vertices = len(bm.verts)
-    bm.free()
-    bpy.ops.object.mode_set(mode="OBJECT")
-
-    if n_vertices != 4:
-        raise Exception(f"expected: 4, actual: {n_vertices}")
+    modes: list[Mode] = ["VERTS", "EDGES", "FACES"]
+    for m in modes:
+        assert_half_wiped_out(m)
 
 
 if __name__ == "__main__":
-    addon_utils.enable("myaddon")
     main()
