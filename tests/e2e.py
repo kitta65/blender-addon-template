a2c703b3196599701e59487bba2af3c806fafb54
dr666m1 <26474260+kitta65@users.noreply.github.com>
2024-03-20 09:23:15 +0000

    add one test case

--- tests/e2e.py
+++ tests/e2e.py
@@ -1,28 +1,24 @@
-from contextlib import contextmanager
 import bpy
+import bmesh
 import addon_utils
 
 
-@contextmanager
-def temp_text(string: str):
-    text = bpy.data.texts.new("temp.py")
-    text.from_string(string)
-    try:
-        yield text
-    finally:
-        bpy.data.texts.remove(text)
+def main():
+    bpy.ops.mesh.primitive_cube_add()
+    bpy.context.scene.tool_settings.mesh_select_mode = [True, False, False]  # vetex
+    bpy.ops.object.mode_set(mode="EDIT")
+    bpy.ops.sample.thanos_wipe_out()
+
+    me = bpy.context.object.data
+    bm = bmesh.from_edit_mesh(me)
+    n_vertices = len(bm.verts)
+    bm.free()
+    bpy.ops.object.mode_set(mode="OBJECT")
+
+    if n_vertices != 4:
+        raise Exception(f"expected: 4, actual: {n_vertices}")
 
 
 if __name__ == "__main__":
     addon_utils.enable("myaddon")
-
-    input_str = "import  bpy\n"
-    expected_str = "import bpy\n"
-    actual_str: str
-
-    with temp_text(input_str) as text:
-        bpy.ops.sample.sample_operator()
-        actual_str = text.as_string()
-
-    if actual_str != expected_str:
-        raise Exception("not formatted!")
+    main()
